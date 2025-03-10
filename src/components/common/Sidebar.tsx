238caffb2bfa8f428b2f309ca9106fc1e3711858
tuyetmai03 <tuyetmai03nd@gmail.com>
2025-03-11 03:42:15 +0700

    feat: thêm layout admin

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -25,7 +25,7 @@ import Title from "antd/es/typography/Title";
 
 const { Header, Content, Sider } = Layout;
 const { Option } = Select;
-const Slidebar = () => {
+const Sidebar = () => {
   return (
     <Sider
       width={260}
@@ -73,4 +73,4 @@ const Slidebar = () => {
   );
 };
 
-export default Slidebar;
+export default Sidebar;
