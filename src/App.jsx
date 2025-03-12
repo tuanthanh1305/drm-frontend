bb9e0a0c6c5163bb9b2fb3366dfae8acd246cbee
suycode <lebaongoc1652004@gmail.com>
2025-03-12 23:05:16 +0700

    update 12/3

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Routes, Route } from "react-router";
+import LayoutAdmin from "./layouts/LayoutAdmin";
+import AdminPage from "./pages/AdminPage";
+import Login from "./pages/Login";
+import TrainingOfficePage from './pages/TrainingOfficePage'
+import Student from "./components/student/Student";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />x
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route index element={<AdminPage />} />
+          <Route path="trainning" element={<TrainingOfficePage />} />
+          <Route path="student" element={<Student />} />
+        </Route>
+      </Routes>
+
+    </>
+  );
+};
+
+export default App;
