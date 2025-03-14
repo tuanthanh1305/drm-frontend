1523fb7477add57983cf7ef945f2f02b2cce215a
longthanh21 <145737956+longthanh21@users.noreply.github.com>
2025-03-14 23:44:05 +0700

    Revert "long commit"
    This reverts commit 4d7479132cdc6d92b1dd41244278925bd5133074.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
+
 import { Routes, Route } from "react-router";
+import LayoutAdmin from "./layouts/LayoutAdmin";
 import AdminPage from "./pages/AdminPage";
 import Login from "./pages/Login";
-import TrainingOfficePage from "./pages/TrainingOfficePage";
-import Student2 from "./components/student/Student2";
-import LayoutStudent from './layouts/LayoutStudent';
-import LayoutAdmin from './layouts/LayoutAdmin';
-import StudentPage from "./pages/StudentPage";
+import TrainingOfficePage from './pages/TrainingOfficePage'
+import Student from "./components/student/Student";
 const App = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/admin" element={<LayoutAdmin />}>
-                    <Route index element={<AdminPage />} />
-                    <Route path="trainning" element={<TrainingOfficePage />} />
-                </Route>
-                <Route path="/student" element={<LayoutStudent />}>
-                    <Route index element={<StudentPage />} />
-                    <Route path="student2" element={<Student2 />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />x
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route index element={<AdminPage />} />
+          <Route path="trainning" element={<TrainingOfficePage />} />
+          <Route path="student" element={<Student />} />
+          <Route path="khanh" element={<PageKhanh></PageKhanh>}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- src/components/student/slidetuan-hung/ActionButtons.jsx
+++ src/components/student/slidetuan-hung/ActionButtons.jsx
@@ -1,20 +1,20 @@
- import React from "react";
- import { Button } from "antd";
- import { LeftOutlined } from "@ant-design/icons";
- 
- const ActionButtons = () => {
-   return (
-     <div className="mt-4 flex justify-end gap-5 max-md:flex-wrap max-sm:flex-col max-sm:items-center">
-       <Button
-         type="primary"
-         icon={<LeftOutlined />}
-         size="large"
-         className="flex items-center gap-2 w-[220px]"
-       >
-         Xuất file PDF
-       </Button>
-     </div>
-   );
- };
- 
- export default ActionButtons;
+import React from "react";
+import { Button } from "antd";
+import { LeftOutlined } from "@ant-design/icons";
+
+const ActionButtons = () => {
+  return (
+    <div className="mt-4 flex justify-end gap-5 max-md:flex-wrap max-sm:flex-col max-sm:items-center">
+      <Button
+        type="primary"
+        icon={<LeftOutlined />}
+        size="large"
+        className="flex items-center gap-2 w-[220px]"
+      >
+        Xuất file PDF
+      </Button>
+    </div>
+  );
+};
+
+export default ActionButtons;
