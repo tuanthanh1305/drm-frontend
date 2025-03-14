03672fdf581b8e2aa72aadbb835d4200c0551a96
longthanh21 <145737956+longthanh21@users.noreply.github.com>
2025-03-14 23:39:53 +0700

    Reapply "long commit"
    This reverts commit c20118f1251c82df36a0643240a558a57dc2d6f2.

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import Student2 from "./components/student/Student2";
 import LayoutStudent from './layouts/LayoutStudent';
 import LayoutAdmin from './layouts/LayoutAdmin';
 import StudentPage from "./pages/StudentPage";
+import StudentManagementPage from "./components/admin/StudentManagementPage";
+
 const App = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/admin" element={<LayoutAdmin />}>
-                    <Route index element={<AdminPage />} />
-                    <Route path="trainning" element={<TrainingOfficePage />} />
-                </Route>
-                <Route path="/student" element={<LayoutStudent />}>
-                    <Route index element={<StudentPage />} />
-                    <Route path="student2" element={<Student2 />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route index element={<AdminPage />} />
+          <Route path="trainning" element={<TrainingOfficePage />} />
+          <Route path="students" element={<StudentManagementPage />} />
+        </Route>
+        <Route path="/student" element={<LayoutStudent />}>
+          <Route index element={<StudentPage />} />
+          <Route path="student2" element={<Student2 />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
