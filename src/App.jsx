858cc138f9266ba2fbf74ed83b658819fdc6ae3f
longthanh21 <145737956+longthanh21@users.noreply.github.com>
2025-03-15 00:11:48 +0700

    ok AppApp

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { Routes, Route } from "react-router";
 import LayoutAdmin from "./layouts/LayoutAdmin";
 import AdminPage from "./pages/AdminPage";
 import Login from "./pages/Login";
-<<<<<<< HEAD
-import TrainingOfficePage from './pages/TrainingOfficePage'
-import Student from "./components/student/Student";
-=======
 import TrainingOfficePage from "./pages/TrainingOfficePage";
 import Student2 from "./components/student/Student2";
 import LayoutStudent from './layouts/LayoutStudent';
@@ -14,19 +10,10 @@ import LayoutAdmin from './layouts/LayoutAdmin';
 import StudentPage from "./pages/StudentPage";
 import StudentManagementPage from "./components/admin/StudentManagementPage";
 
->>>>>>> parent of c20118f (Revert "long commit")
 const App = () => {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/login" element={<Login />} />x
-        <Route path="/admin" element={<LayoutAdmin />}>
-          <Route index element={<AdminPage />} />
-          <Route path="trainning" element={<TrainingOfficePage />} />
-          <Route path="student" element={<Student />} />
-          <Route path="khanh" element={<PageKhanh></PageKhanh>}></Route>
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<LayoutAdmin />}>
           <Route index element={<AdminPage />} />
@@ -36,7 +23,6 @@ const App = () => {
         <Route path="/student" element={<LayoutStudent />}>
           <Route index element={<StudentPage />} />
           <Route path="student2" element={<Student2 />} />
->>>>>>> parent of c20118f (Revert "long commit")
         </Route>
       </Routes>
     </>
